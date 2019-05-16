78a93353027248172211fbd15ad1bd232336a97d
AngelicaS <angiesacala@gmail.com>
2019-05-16 08:49:19 +0800

    fix(problemset): add solutions to problem 3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // solutions here
 var events = require('events');
 var lib = require('./lib/lib');
+var genericPool = require("generic-pool");
+var mysql = require("mysql");
+var myPool;
 
+//Solution to #1
 //Will contain the tasks to run in parallel to each other
 const parallelTasks = [];
 
@@ -27,7 +31,7 @@ async function doAsync(array, parallel, callback) {
   }
 }
 
-
+//Solution to #2
 class RandStringSource extends events.EventEmitter {
   constructor(x) {
     super();
@@ -54,9 +58,53 @@ class RandStringSource extends events.EventEmitter {
   }
 }
 
+//Solution to #3
+class ResourceManager {
+  constructor(x) {
+    const factory = {
+      create: function() {
+         return mysql.createConnection({
+           port: 3306,
+           host: 'localhost',
+           user: 'root',
+           pass: 'admin',
+           db: 'test_db'
+         });
+      },
+      destroy: function(client) {
+         client.end();
+      }
+    };
+
+    const opts = {
+      max: x
+    };
+
+    myPool = genericPool.createPool(factory, opts);
+  }
+
+  borrow(callback) {
+    const resourcePromise = myPool.acquire();
+
+    resourcePromise
+      .then(function(client) {
+        callback(client);
+
+        //Release the borrowed resource
+        client.release = function() {
+          myPool.release(client)
+        };
+      })
+      .catch(function(err) {
+        console.log("Error");
+      });
+  }
+}
+
 
 
 module.exports = {
   doAsync,
-  RandStringSource
+  RandStringSource,
+  ResourceManager
 };
